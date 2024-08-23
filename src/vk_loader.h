393088178b96d80b932ba49a620810c4c6779f85
eduameli <eduamelian@gmail.com>
2024-08-24 01:00:21 +0200

    dirty file loading setup, also vma and others were never build build as a static lib they were empty

---
+++ src/vk_loader.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "aurora_pch.h"
+#include "vk_types.h"
+#include <filesystem>
+
+namespace Aurora {
+
+// forward declaration
+class Engine;
+
+struct GeoSurface
+{
+  uint32_t startIndex;
+  uint32_t count;
+};
+
+struct MeshAsset
+{
+  std::string name;
+  std::vector<GeoSurface> surfaces;
+  GPUMeshBuffers meshBuffers;
+};
+
+
+
+} // namespace aurora
+
+
