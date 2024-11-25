132aad23f783c848ee13e079196c13f45fa9c0eb
eduameli <eduamelian@gmail.com>
2024-11-25 15:33:45 +0000

    depth? not working bindless structure..

--- src/engine.cpp
+++ src/engine.cpp
@@ -1695,23 +1695,28 @@ void Engine::init_pipelines()
 
   PipelineBuilder b;
   b.set_shaders(bindlessVert, bindlessFrag);
-  // b.set_input_topology(VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST);
-  // b.set_polygon_mode(VK_POLYGON_MODE_FILL);
-  // b.set_multisampling_none();
-  // b.disable_blending();
-  // b.set_cull_mode(VK_CULL_MODE_BACK_BIT, VK_FRONT_FACE_COUNTER_CLOCKWISE);
   b.set_color_attachment_format(m_DrawImage.imageFormat);
-  b.set_depth_format(m_DepthImage.imageFormat);
+  // b.set_depth_format(m_DepthImage.imageFormat);
 
   
+  // b.set_input_topology(VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST);
+  // b.set_polygon_mode(VK_POLYGON_MODE_FILL);
+  // b.set_cull_mode(VK_CULL_MODE_BACK_BIT, VK_FRONT_FACE_COUNTER_CLOCKWISE); // NOTE: backface culling?
+  // //pipelineBuilder.set_cull_mode(VK_CULL_MODE_NONE, VK_FRONT_FACE_CLOCKWISE);
+  // b.set_multisampling_none();
+  // b.disable_blending(); // need transparent alternative?
+  // b.enable_depthtest(true, VK_COMPARE_OP_EQUAL); // NOTE: bigger?
+
+
+  
+  b.set_depth_format(m_DepthImage.imageFormat);
   b.set_input_topology(VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST);
   b.set_polygon_mode(VK_POLYGON_MODE_FILL);
-  b.set_cull_mode(VK_CULL_MODE_BACK_BIT, VK_FRONT_FACE_COUNTER_CLOCKWISE); // NOTE: backface culling?
-  //pipelineBuilder.set_cull_mode(VK_CULL_MODE_NONE, VK_FRONT_FACE_CLOCKWISE);
+  b.set_cull_mode(VK_CULL_MODE_BACK_BIT, VK_FRONT_FACE_COUNTER_CLOCKWISE);
   b.set_multisampling_none();
   b.disable_blending();
-  b.enable_depthtest(true, VK_COMPARE_OP_EQUAL); // NOTE: bigger?
-
+  b.enable_depthtest(true, VK_COMPARE_OP_GREATER_OR_EQUAL);
+  
   
   b.PipelineLayout = bindless_pipeline_layout;
   std_pipeline = b.build_pipeline(device);
